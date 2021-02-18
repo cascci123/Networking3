9fa7efbf392dc061a01ce7b74ef3912a769e2fb7
cascci123 <71568834+cascci123@users.noreply.github.com>
2021-02-17 18:00:10 -0800

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
-#import solution module
+# import solution module
 from socket import *
 import sys  # In order to terminate the program
 
 
-def webServer(port):
+def webserver(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
     # Prepare a sever socket
-    #HOST = '127.0.0.1'
-    #PORT = 13331
+    # HOST = '127.0.0.1'
+    # PORT = 13331
     # Fill in start
     serverSocket.bind(('127.0.0.1', port))
     # Fill in end
@@ -16,12 +16,12 @@ def webServer(port):
     while True:
         # Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() # Fill in start      #Fill in end
+        connectionSocket, addr = serverSocket.accept()  # Fill in start      #Fill in end
         try:
-            message =  connectionSocket.recv(1024)# Fill in start    #Fill in end
+            message = connectionSocket.recv(1024)  # Fill in start    #Fill in end
             filename = message.split()[1]
             f = open(filename[1:])
-            outputdata =  f.read()# Fill in start     #Fill in end
+            outputdata = f.read()  # Fill in start     #Fill in end
 
             # Send one HTTP header line into socket
             # Fill in start
@@ -35,13 +35,13 @@ def webServer(port):
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-    # Send response message for file not found (404)
-    # Fill in start
+            # Send response message for file not found (404)
+            # Fill in start
             connectionSocket.send('HTTP 1.1 404 Not Found')
-    # Fill in end
+        # Fill in end
 
-    # Close client socket
-    # Fill in start
+        # Close client socket
+        # Fill in start
         connectionSocket.close()
     # Fill in end
 
@@ -50,10 +50,4 @@ def webServer(port):
 
 
 if __name__ == "__main__":
-    webServer(13331)
-
-
-
-
-
-
+    webserver(13331)
